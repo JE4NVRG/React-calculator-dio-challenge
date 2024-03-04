6a7cd132a2388527be0626c0b7224d7b5faee772
Jean Carlos Vargas <106420077+JE4NVRG@users.noreply.github.com>
2024-03-04 20:37:16 +0000

    adicionado `botoes` e criada algumas `funcoes`

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,7 @@
-const Button = () => {
-  return <div className="App">Ola</div>;
+import { ButtonContainer } from "./styles";
+
+const Button = ({ label, onClick }) => {
+  return <ButtonContainer onClick={onClick}>{label}</ButtonContainer>;
 };
 
 export default Button;
